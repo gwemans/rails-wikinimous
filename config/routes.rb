83143aa1a83f94fd4d361998d1b4fd9150a89a42
gerardo wemans <gerardo.wemans@gmail.com>
2022-11-15 17:09:47 +0000

    resolved exercise

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :articles
+
+  # show articlelist
+  get '/articles', to: 'articles#index'
+  # create new article
+  get '/articles/new', to: 'articles#new', as: :new_article
+  post '/articles', to: 'articles#create'
+  # show one article's details
+  get '/articles/:id', to: 'articles#show', as: :article
+  # update a article
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch '/articles/:id', to: 'articles#update'
+  # delete a article
+  delete '/articles/:id', to: 'articles#destroy'
 end
